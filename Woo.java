3f72c1dc86fd2cf28e811836bd275d1492ffc735
jerry1ye10 <jerry1ye10@gmail.com>
2018-06-06 08:56:51 -0400

    updated driver

--- Woo.java
+++ Woo.java
@@ -1,5 +1,10 @@
 public class Woo{//driver
   public static void main(String[] args){
+    //seed data from CSVs
+    System.out.println("Welcome to Flying Bob's Programming Program");
+    System.out.println("Username?");
+    //import scanner for user input
+    System.out.println("Pass?");
     
   }
 }
