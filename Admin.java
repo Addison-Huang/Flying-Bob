c61caba901f43dc021bc83e6c0bbdbcd0b8d2571
jerry1ye10 <jerry1ye10@gmail.com>
2018-06-07 16:02:48 -0400

    fix admin

--- Admin.java
+++ Admin.java
@@ -4,6 +4,7 @@ public class Admin extends User {
         super(u, p);
     }
 
+<<<<<<< Updated upstream
      public Course addCourse(Course c) {
          System.out.println(LCourse.addCourse(c));
          return c;
@@ -13,11 +14,23 @@ public class Admin extends User {
          System.out.println(LCourse.removeCourse(c));
          return c;
      }
+=======
+  public Course addCourse(Course c, LCourse L) {
+	System.out.println(L.addCourse(c));
+	return c;
+    }
+
+  public Course removeCourse(Course c, LCourse L) {
+	System.out.println(L.removeCourse(c));
+	return c;
+    }
+>>>>>>> Stashed changes
 
      public void addStudent(Student s, Course c) {
          c.addStudent(s);
      }
 
+<<<<<<< Updated upstream
      public Student removeStudent(int id) {
          return LStudent.removeStudent(id);
      }
@@ -25,6 +38,15 @@ public class Admin extends User {
      public Student getStudent(int id) {
          return LStudent.getStudent(id);
      }
+=======
+  public Student removeStudent(int id, LStudent L) {
+        return L.removeStudent(id);
+    }
+
+  public Student getStudent(int id, LStudent L) {
+        return L.getStudent(int id);
+    }
+>>>>>>> Stashed changes
 
     public  boolean addStudentInfo(String info) {
         return true;
